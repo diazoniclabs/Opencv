93071617487b7882ad2e90e695ddd7960b302cea
ameervalki <52085350+ameervalki@users.noreply.github.com>
2020-01-23 22:44:38 +0530

    Update P1 - Solid Color.py

--- P1 - Solid Color.py
+++ P1 - Solid Color.py
@@ -1,4 +1,4 @@
-import cv2 # importing opencv library
+import cv2 # importing computer vision library
 import numpy as np  #importing numerical python library
 
 img = np.ones([250,250,3],dtype = np.uint8) 
@@ -6,7 +6,7 @@ img = np.ones([250,250,3],dtype = np.uint8)
 ''' Taking white background image , VERY IMPORTANT to mention dtype as np.uint8,
 by default it will be float64 which wont work for mixing of more than 2 colors'''
 
-img[:,:] = 150,100,100 # mention BGR values here, Use Color Picker from Paint for reference
+img[:,:] = 150,100,100 # Taking all rows and columns of pixels and assigning it to BGR values here, Use Color Picker from Paint for reference
 
 cv2.imshow('Output Color',img)  # Display the image
 
